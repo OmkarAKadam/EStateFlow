510057e9667062f2384ca859e0c5bbfa142bbe61
Omkar Kadam <kaizenki572@gmail.com>
2026-03-24 17:12:01 +0530

    remove dotenv, use env variables

--- estateflow-backend/src/main/java/com/estateflow/estateflowbackend/EstateflowBackendApplication.java
+++ estateflow-backend/src/main/java/com/estateflow/estateflowbackend/EstateflowBackendApplication.java
@@ -1,6 +1,5 @@
 package com.estateflow.estateflowbackend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,23 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EstateflowBackendApplication {
 
     public static void main(String[] args) {
-
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
-        String dbUrl = dotenv.get("DB_URL");
-        String dbUser = dotenv.get("DB_USERNAME");
-        String dbPass = dotenv.get("DB_PASSWORD");
-        String jwtSecret = dotenv.get("JWT_SECRET");
-        String portStr = dotenv.get("PORT");
-
-        if (portStr != null && !portStr.isBlank()) {
-            System.setProperty("PORT", portStr);
-        }
-        if (dbUrl != null) System.setProperty("DB_URL", dbUrl);
-        if (dbUser != null) System.setProperty("DB_USERNAME", dbUser);
-        if (dbPass != null) System.setProperty("DB_PASSWORD", dbPass);
-        if (jwtSecret != null) System.setProperty("JWT_SECRET", jwtSecret);
-
         SpringApplication.run(EstateflowBackendApplication.class, args);
     }
 
